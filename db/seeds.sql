eb15c52495877dd7264e75f805cbd67533347a18
mateyo3 <mateoreyes720@gmail.com>
2017-11-23 14:06:28 -0800

    initial file setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Shack Stack');
+INSERT INTO burgers (burger_name) VALUES ('Shroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quarter pounder with cheese');
